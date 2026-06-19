8e5d02c8f6b88083372829de246255592c1220e0
Ishowspeed69696969 <speedyboiz69@gmail.com>
2026-06-19 20:48:05 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,1040 @@
+// ==========================================
+// DATA.JS
+// Dữ liệu trường - ngành - tổ hợp
+// ==========================================
+
+
+const admissionData = {
+
+
+
+SPS:{
+
+
+
+// =======================
+// SƯ PHẠM TOÁN
+// =======================
+
+
+"7140209":{
+
+
+name:"Sư phạm Toán học",
+
+
+combos:{
+
+
+
+"A00TO":{
+
+base:"A00",
+
+dg:"Toán",
+
+hb1:"Vật lý",
+
+hb2:"Hóa học"
+
+},
+
+
+
+"A01TO":{
+
+base:"A01",
+
+dg:"Toán",
+
+hb1:"Vật lý",
+
+hb2:"Tiếng Anh"
+
+},
+
+
+
+"A02TO":{
+
+base:"A02",
+
+dg:"Toán",
+
+hb1:"Vật lý",
+
+hb2:"Sinh học"
+
+},
+
+
+
+"B00TO":{
+
+base:"B00",
+
+dg:"Toán",
+
+hb1:"Hóa học",
+
+hb2:"Sinh học"
+
+},
+
+
+
+"C01TO":{
+
+base:"C01",
+
+dg:"Toán",
+
+hb1:"Ngữ văn",
+
+hb2:"Vật lý"
+
+},
+
+
+
+"C03TO":{
+
+base:"C03",
+
+dg:"Toán",
+
+hb1:"Ngữ văn",
+
+hb2:"Lịch sử"
+
+},
+
+
+
+"C04TO":{
+
+base:"C04",
+
+dg:"Toán",
+
+hb1:"Ngữ văn",
+
+hb2:"Địa lí"
+
+},
+
+
+
+"D01TO":{
+
+base:"D01",
+
+dg:"Toán",
+
+hb1:"Ngữ văn",
+
+hb2:"Tiếng Anh"
+
+},
+
+
+
+"D07TO":{
+
+base:"D07",
+
+dg:"Toán",
+
+hb1:"Hóa học",
+
+hb2:"Tiếng Anh"
+
+},
+
+
+
+"D08TO":{
+
+base:"D08",
+
+dg:"Toán",
+
+hb1:"Sinh học",
+
+hb2:"Tiếng Anh"
+
+},
+
+
+
+"X01TO":{
+
+base:"X01",
+
+dg:"Toán",
+
+hb1:"Ngữ văn",
+
+hb2:"GDKTPL"
+
+},
+
+
+
+"X06TO":{
+
+base:"X06",
+
+dg:"Toán",
+
+hb1:"Vật lí",
+
+hb2:"Tin học"
+
+},
+
+
+
+"X26TO":{
+
+base:"X26",
+
+dg:"Toán",
+
+hb1:"Tin học",
+
+hb2:"Tiếng Anh"
+
+}
+
+
+}
+
+
+},
+
+
+
+
+
+
+
+// =======================
+// SƯ PHẠM VẬT LÝ
+// =======================
+
+
+"7140211":{
+
+
+name:"Sư phạm Vật lý",
+
+
+combos:{
+
+
+"A00LY":{
+
+base:"A00",
+
+dg:"Vật lí",
+
+hb1:"Toán",
+
+hb2:"Hóa học"
+
+},
+
+
+"A01LY":{
+
+base:"A01",
+
+dg:"Vật lí",
+
+hb1:"Toán",
+
+hb2:"Tiếng Anh"
+
+},
+
+
+"A02LY":{
+
+base:"A02",
+
+dg:"Vật lí",
+
+hb1:"Toán",
+
+hb2:"Sinh học"
+
+},
+
+
+"C01LY":{
+
+base:"C01",
+
+dg:"Vật lí",
+
+hb1:"Ngữ văn",
+
+hb2:"Toán"
+
+},
+
+
+"D01LY":{
+
+base:"D01",
+
+dg:"Vật lí",
+
+hb1:"Ngữ văn",
+
+hb2:"Tiếng Anh"
+
+},
+
+
+"X06LY":{
+
+base:"X06",
+
+dg:"Vật lí",
+
+hb1:"Toán",
+
+hb2:"Tin học"
+
+},
+
+
+"X07LY":{
+
+base:"X07",
+
+dg:"Vật lí",
+
+hb1:"Toán",
+
+hb2:"Công nghệ công nghiệp"
+
+},
+
+
+"X08LY":{
+
+base:"X08",
+
+dg:"Vật lí",
+
+hb1:"Toán",
+
+hb2:"Công nghệ nông nghiệp"
+
+}
+
+
+}
+
+
+
+},
+
+
+
+
+
+
+
+// =======================
+// SƯ PHẠM HÓA HỌC
+// =======================
+
+
+"7140212":{
+
+
+name:"Sư phạm Hóa học",
+
+
+combos:{
+
+
+"A00HO":{
+
+base:"A00",
+
+dg:"Hóa học",
+
+hb1:"Toán",
+
+hb2:"Vật lí"
+
+},
+
+
+"B00HO":{
+
+base:"B00",
+
+dg:"Hóa học",
+
+hb1:"Toán",
+
+hb2:"Sinh học"
+
+},
+
+
+"D07HO":{
+
+base:"D07",
+
+dg:"Hóa học",
+
+hb1:"Toán",
+
+hb2:"Tiếng Anh"
+
+},
+
+
+"X10HO":{
+
+base:"X10",
+
+dg:"Hóa học",
+
+hb1:"Toán",
+
+hb2:"Tin học"
+
+}
+
+
+
+}
+
+
+},
+
+
+
+
+
+
+// =======================
+// SƯ PHẠM SINH
+// =======================
+
+
+"7140213":{
+
+
+name:"Sư phạm Sinh học",
+
+
+combos:{
+
+
+"B00SI":{
+
+base:"B00",
+
+dg:"Sinh học",
+
+hb1:"Toán",
+
+hb2:"Hóa học"
+
+},
+
+
+"D08SI":{
+
+base:"D08",
+
+dg:"Sinh học",
+
+hb1:"Toán",
+
+hb2:"Tiếng Anh"
+
+},
+
+
+"X14SI":{
+
+base:"X14",
+
+dg:"Sinh học",
+
+hb1:"Toán",
+
+hb2:"Tin học"
+
+},
+
+
+"X16SI":{
+
+base:"X16",
+
+dg:"Sinh học",
+
+hb1:"Toán",
+
+hb2:"Công nghệ nông nghiệp"
+
+}
+
+
+
+}
+
+
+},
+
+// =======================
+// SƯ PHẠM NGỮ VĂN
+// =======================
+
+
+"7140217":{
+
+
+name:"Sư phạm Ngữ văn",
+
+
+combos:{
+
+
+"C00VA":{
+
+base:"C00",
+
+dg:"Ngữ văn",
+
+hb1:"Lịch sử",
+
+hb2:"Địa lí"
+
+},
+
+
+"C03VA":{
+
+base:"C03",
+
+dg:"Ngữ văn",
+
+hb1:"Toán",
+
+hb2:"Lịch sử"
+
+},
+
+
+"C04VA":{
+
+base:"C04",
+
+dg:"Ngữ văn",
+
+hb1:"Toán",
+
+hb2:"Địa lí"
+
+},
+
+
+"D01VA":{
+
+base:"D01",
+
+dg:"Ngữ văn",
+
+hb1:"Toán",
+
+hb2:"Tiếng Anh"
+
+},
+
+
+"D14VA":{
+
+base:"D14",
+
+dg:"Ngữ văn",
+
+hb1:"Lịch sử",
+
+hb2:"Tiếng Anh"
+
+},
+
+
+"D15VA":{
+
+base:"D15",
+
+dg:"Ngữ văn",
+
+hb1:"Địa lí",
+
+hb2:"Tiếng Anh"
+
+},
+
+
+"X01VA":{
+
+base:"X01",
+
+dg:"Ngữ văn",
+
+hb1:"Toán",
+
+hb2:"GDKTPL"
+
+},
+
+
+"X70VA":{
+
+base:"X70",
+
+dg:"Ngữ văn",
+
+hb1:"Lịch sử",
+
+hb2:"GDKTPL"
+
+},
+
+
+"X74VA":{
+
+base:"X74",
+
+dg:"Ngữ văn",
+
+hb1:"Địa lí",
+
+hb2:"GDKTPL"
+
+},
+
+
+"X78VA":{
+
+base:"X78",
+
+dg:"Ngữ văn",
+
+hb1:"Tiếng Anh",
+
+hb2:"GDKTPL"
+
+}
+
+
+
+}
+
+
+},
+
+
+
+
+
+
+
+// =======================
+// SƯ PHẠM TIẾNG ANH
+// =======================
+
+
+"7140231":{
+
+
+name:"Sư phạm Tiếng Anh",
+
+
+combos:{
+
+
+"D01TA":{
+
+base:"D01",
+
+dg:"Tiếng Anh",
+
+hb1:"Ngữ văn",
+
+hb2:"Toán"
+
+},
+
+
+"D09TA":{
+
+base:"D09",
+
+dg:"Tiếng Anh",
+
+hb1:"Toán",
+
+hb2:"Lịch sử"
+
+},
+
+
+"D14TA":{
+
+base:"D14",
+
+dg:"Tiếng Anh",
+
+hb1:"Ngữ văn",
+
+hb2:"Lịch sử"
+
+},
+
+
+"D15TA":{
+
+base:"D15",
+
+dg:"Tiếng Anh",
+
+hb1:"Ngữ văn",
+
+hb2:"Địa lí"
+
+},
+
+
+"X78TA":{
+
+base:"X78",
+
+dg:"Tiếng Anh",
+
+hb1:"Ngữ văn",
+
+hb2:"GDKTPL"
+
+},
+
+
+"X79TA":{
+
+base:"X79",
+
+dg:"Tiếng Anh",
+
+hb1:"Ngữ văn",
+
+hb2:"Tin học"
+
+}
+
+
+}
+
+
+
+},
+
+
+
+
+
+
+
+
+// =======================
+// GIÁO DỤC TIỂU HỌC
+// =======================
+
+
+"7140202":{
+
+
+name:"Giáo dục Tiểu học",
+
+
+combos:{
+
+
+"A00TH":{
+
+base:"A00",
+
+dg:"Toán",
+
+hb1:"Vật lí",
+
+hb2:"Hóa học"
+
+},
+
+
+"C00TH":{
+
+base:"C00",
+
+dg:"Ngữ văn",
+
+hb1:"Lịch sử",
+
+hb2:"Địa lí"
+
+},
+
+
+"D01TH":{
+
+base:"D01",
+
+dg:"Toán",
+
+hb1:"Ngữ văn",
+
+hb2:"Tiếng Anh"
+
+},
+
+
+"X01TH":{
+
+base:"X01",
+
+dg:"Ngữ văn",
+
+hb1:"Toán",
+
+hb2:"GDKTPL"
+
+}
+
+
+
+}
+
+
+},
+
+
+
+
+
+
+
+// =======================
+// GIÁO DỤC MẦM NON
+// =======================
+
+
+"7140201":{
+
+
+name:"Giáo dục Mầm non",
+
+
+combos:{
+
+
+"M02MN":{
+
+base:"M02",
+
+dg:"Toán",
+
+hb1:"NK GDMN 1",
+
+hb2:"NK GDMN 2"
+
+},
+
+
+"M03MN":{
+
+base:"M03",
+
+dg:"Ngữ văn",
+
+hb1:"NK GDMN 1",
+
+hb2:"NK GDMN 2"
+
+}
+
+
+
+}
+
+
+},
+
+
+
+
+
+
+
+// =======================
+// GIÁO DỤC THỂ CHẤT
+// =======================
+
+
+"7140206":{
+
+
+name:"Giáo dục Thể chất",
+
+
+combos:{
+
+
+"M08TD":{
+
+base:"M08",
+
+dg:"Ngữ văn",
+
+hb1:"NK TDTT 1",
+
+hb2:"NK TDTT 2"
+
+},
+
+
+"T01TD":{
+
+base:"T01",
+
+dg:"Toán",
+
+hb1:"NK TDTT 1",
+
+hb2:"NK TDTT 2"
+
+},
+
+
+"Q01TD":{
+
+base:"Q01",
+
+dg:"Toán",
+
+hb1:"NK TDTT 1",
+
+hb2:"NK GDQP-AN"
+
+},
+
+
+"Q02TD":{
+
+base:"Q02",
+
+dg:"Ngữ văn",
+
+hb1:"NK TDTT 1",
+
+hb2:"NK GDQP-AN"
+
+}
+
+
+
+}
+
+
+},
+
+
+
+
+
+
+
+// =======================
+// NGÔN NGỮ ANH
+// =======================
+
+
+"7220201":{
+
+
+name:"Ngôn ngữ Anh",
+
+
+combos:{
+
+
+"D01TA":{
+
+base:"D01",
+
+dg:"Tiếng Anh",
+
+hb1:"Ngữ văn",
+
+hb2:"Toán"
+
+},
+
+
+"D14TA":{
+
+base:"D14",
+
+dg:"Tiếng Anh",
+
+hb1:"Ngữ văn",
+
+hb2:"Lịch sử"
+
+},
+
+
+"D15TA":{
+
+base:"D15",
+
+dg:"Tiếng Anh",
+
+hb1:"Ngữ văn",
+
+hb2:"Địa lí"
+
+},
+
+
+"X79TA":{
+
+base:"X79",
+
+dg:"Tiếng Anh",
+
+hb1:"Ngữ văn",
+
+hb2:"Tin học"
+
+}
+
+
+
+}
+
+
+},
